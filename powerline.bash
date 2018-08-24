def5f25e3943347914d21724bc63986f2ba4caf6
wndhydrnt <hydrantanderwand@gmail.com>
2018-08-24 20:48:10 +0200

    Separate powerline prompt and kubectl prompt plugin

---
+++ powerline.bash
@@ -0,0 +1,1 @@
+POWERLINE_RIGHT_PROMPT="k8s_context in_vim clock battery user_info"
--- powerline_kubectl.bash
+++ powerline_kubectl.bash
@@ -1,12 +1,10 @@
-function __powerline_k8s_prompt {
-  local k8s_version=""
+function __powerline_k8s_context_prompt {
+  local k8s_context=""
 
   if command -v kubectl &>/dev/null
   then
-    k8s_version=$(kubectl config current-context)
+    k8s_context=$(kubectl config current-context)
   fi
 
-  [[ -n "${k8s_version}" ]] && echo "${k8s_version}|141"
+  [[ -n "${k8s_context}" ]] && echo "${k8s_context}|141"
 }
-
-POWERLINE_RIGHT_PROMPT="k8s in_vim clock battery user_info"
