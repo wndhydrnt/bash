76500b892ee54e0d7b8e6f29376cc3d93a8b4aa9
Markus Meyer <m.meyer@mytaxi.com>
2018-08-27 12:46:06 +0200

    Display go version if .go files are in cwd

--- powerline_go_version.bash
+++ powerline_go_version.bash
@@ -1,9 +1,11 @@
 function __powerline_go_version_prompt {
   local go_version=""
 
-  if command -v go &>/dev/null && [[ -f "$PWD/Gopkg.toml" ]]
-  then
-    go_version=$(go version | cut -d ' ' -f 3)
+  if command -v go &>/dev/null; then
+    local go_file_count=$(find . -name '*.go' -maxdepth 1 | wc -l)
+    if [[ "${go_file_count}" -gt "0" ]]; then
+      go_version=$(go version | cut -d ' ' -f 3)
+    fi
   fi
 
   [[ -n "${go_version}" ]] && echo "${go_version}|71"
