0094e2d971cb8b2b7a5be646ea83112ef3dcf77f
wndhydrnt <hydrantanderwand@gmail.com>
2019-02-06 18:18:13 +0100

    Improve minikube bash completion

--- minikube.bash
+++ minikube.bash
@@ -1,6 +1,3 @@
 if command -v minikube &>/dev/null; then
-  if [[ ! -f "${HOME}/.minikube-completion" ]]; then
-    minikube completion bash > "${HOME}/.minikube-completion"
-  fi
-  source "${HOME}/.minikube-completion"
+  source <(minikube completion bash)
 fi
