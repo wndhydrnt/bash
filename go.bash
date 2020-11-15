24336ceda3575374c22f2884bdc786a468f717e2
wndhydrnt <hydrantanderwand@gmail.com>
2020-11-15 11:24:27 +0100

    Fix broken detection after upgrade of asdf to latest

--- go.bash
+++ go.bash
@@ -1,6 +1,6 @@
 if command -v asdf &>/dev/null
 then
-  export GO_VERSION=$(asdf current golang | cut -d ' ' -f 1)
+  export GO_VERSION=$(asdf current golang | tr -s ' ' | cut -d ' ' -f 2)
   export GOROOT=$(asdf where golang ${GO_VERSION})/go
   export PATH="$GOROOT/bin:$PATH"
   export GOPATH=$HOME/dev/workspace/go
